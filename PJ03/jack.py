993bab3c2b29df32cd5f2a4881ee1bc926a3c04f
Justin Reynolds <Jreynolds97@comcast.net>
2018-10-26 10:09:46 -0600

    Update with new code

--- PJ03/jack.py
+++ PJ03/jack.py
@@ -8,7 +8,15 @@ jackfile_folder = os.path.join(cwd,"jack_files")
 
 SY_OP = ['+', '*', '/', '&', '|', '<', '>']
 
-def get_col(in_char):
+def tranform(in_char):
+    """Transform maps tokens to their state name to make it easier
+    to read the state table
+
+    Parameters
+    ----------
+    in_char : character
+        The character to map to a state name
+    """
     in_char = str(in_char)
     if(in_char in ['\r', '\n', '\r\n']):
         return 'NEWLINE'
@@ -55,50 +63,87 @@ def get_col(in_char):
     return 'BADTOKEN'
 
 if __name__ == '__main__':
+    # Make the results folder if it doesn't exist
     if not os.path.exists(results_folder):
         os.makedirs(results_folder)
+    # Loop through each of the .jack files in the jackfile folder
     for jackfile in os.listdir(jackfile_folder):
+        # Decode the filename if it has special OS encoding
         filename = os.fsdecode(jackfile)
         with open(os.path.join(jackfile_folder,filename)) as f:
-            print(filename)
+            # Initial setup of the DFA with an empty char, and the initial state
             tokens = []
             current_char = ' '
-            col = 'INITIAL'
+            char_transition = 'INITIAL'
             current_state = 'INITIAL'
             prev_state = 'INITIAL'
             current_token = ''
-            x = 0
         
+            # Read the first character from the file
             current_char = f.read(1)
             while True:
+                # Detect if the character was empty, as that is how Python represensts EOF
                 if current_char == '':
                     break
-                col = get_col(current_char)
+                # Get the character transiton from the transform function
+                char_transition = tranform(current_char)
+                # The logic to keep the transition table small. 
+                # This is akin to the logic that I used in PJ01 for the trap state transitions
                 try:
-                    current_state = transitions[current_state][col]
+                    # If there exists a proper transition for the current state and char, take it
+                    current_state = transitions[current_state][char_transition]
                 except:
+                    # Otherwise, try getting the '*' transition from the table
                     try:
                         current_state = transitions[current_state]['*']
                     except:
+                    # If all else fails, the machine doesn't have an explicit transition for the
+                    # character, and doesn't have a redirected wildcard, so return to the initial
+                    # state to read the next token properly
                         current_state = 'INITIAL'
-                # print(current_char, current_token, current_state)
-                I=0
+
+                # If the current state the machine is in is the INITIAL state, then we've 
+                # finished a token, and need to determine what category the token is in
                 if(current_state == 'INITIAL'):
+                    # The previous state variable is what allows for maxmunch and for
+                    # the distinguishing of token type. 
+
+                    # If the state is one of the unique SLASH or STAR states, then it's a 
+                    # system operator
                     if(prev_state in ['SLASH', 'STAR']):
                         tokens.append((current_token, 'SY_OP'))
+                    # If it's a comment we ignore it in the write out
                     elif('COMMENT' in prev_state):
                         continue
+                    # There are many identifier states that are used for distinguishing
+                    # different identifiers within the keywords. So if we're in ANY of those
+                    # states, we have an identifier
                     elif('IDENT' in prev_state):
                         tokens.append((current_token, 'IDENT'))
+                    # The same with identifiers is true for integers
                     elif('INTEGER' in prev_state):
                         tokens.append((current_token, 'INTEGER'))
+                    # If the previous state was a space character, just ignore it. Otherwise
+                    # write the full token and the previous state to the token list
                     elif(prev_state not in ['SPACE', 'NEWLINE']):
                         tokens.append((current_token, prev_state))
+                    # After writing a token (or not) to the token file, reset the token 
+                    # to an empty character to properly be able to read the next token. 
+                    # This ALSO does not read from the file, which allows us to us the
+                    # character that we just read to determine where it would go
+                    # properly. 
                     current_token = ''
                 else:
+                    # If we haven't returned to the initial state, we're still in the same
+                    # maxed munched token, so we append the character to the token and 
+                    # read the next character from the file
                     current_token += current_char
                     current_char = f.read(1)
+                # remember where we are in the previous state variable so if we reach the end
+                # of the token, we remember what token type we read
                 prev_state = current_state
+
+            # Do the writing logic again, but at the very end of the file
             if(current_state not in ['SPACE','NEWLINE'] and current_token != ''):
                 if(prev_state in ['SLASH', 'STAR']):
                     tokens.append((current_token, 'SY_OP'))
@@ -113,4 +158,4 @@ if __name__ == '__main__':
 
         with open(os.path.join(results_folder,"{}.tok".format(filename.replace('.jack',''))),'w+') as tok_file:
             for token in tokens:
-                tok_file.write("{}\t{}\n".format(token[1],token[0]))
+                tok_file.write("{},{}\n".format(token[1],token[0]))
